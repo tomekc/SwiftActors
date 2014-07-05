f26c388ea8e03aac3b434e210a0c157d066322ef
Tomek Cejner <tomek@smartrecruiters.com>
2014-07-05 22:40:04 +0200

    Initial Commit

---
+++ Swactor/Swactor.h
@@ -0,0 +1,19 @@
+//
+//  Swactor.h
+//  Swactor
+//
+//  Created by Tomek on 05.07.2014.
+//  Copyright (c) 2014 Tomek Cejner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swactor.
+FOUNDATION_EXPORT double SwactorVersionNumber;
+
+//! Project version string for Swactor.
+FOUNDATION_EXPORT const unsigned char SwactorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swactor/PublicHeader.h>
+
+
